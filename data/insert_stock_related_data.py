78b26271ed0540da04481802c91baf3d0eb79c7d
Neel <neelaryan2@gmail.com>
2021-03-19 00:07:22 +0530

    data insertion scripts added

---
+++ data/insert_stock_related_data.py
@@ -0,0 +1,165 @@
+import sys, os, django
+
+project_dir = "../"
+sys.path.append(project_dir)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StockMarket.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+import market.models as models
+from tqdm import tqdm
+import random
+import yfinance as yf
+import pandas as pd
+from pytickersymbols import PyTickerSymbols
+
+
+def get_index_data():
+    stock_data = PyTickerSymbols()
+    indices = stock_data.get_all_indices()
+
+    bad = '-'
+    exchanges = set()
+    companies = {}
+    index_exchange = {}
+    composition = {}
+    stock_listed_at = {}
+
+    prefs = ['BME', 'LON', 'OTCMKTS']
+
+    for index in indices:
+        cur = list(stock_data.get_stocks_by_index(index))
+        current_exchanges = set()
+        for stock in cur:
+            ex_variations = [(s['yahoo'], s['google'].split(':')[0]) for s in stock['symbols'] if s['google'].split(':')[0] != bad]
+
+            sym = stock['symbol']
+            if not sym or not ex_variations:
+                continue
+            sym = sym.split(':')[-1]
+
+            if index not in composition.keys():
+                composition[index] = []
+            composition[index].append(sym)
+
+            if sym not in companies.keys():
+                companies[sym] = {'yahoo,exchange': set()}
+                assert stock['name']
+                assert stock['country']
+                companies[sym]['name'] = stock['name']
+                companies[sym]['country'] = stock['country']
+
+            if sym not in stock_listed_at.keys():
+                stock_listed_at[sym] = set()
+
+            for e in ex_variations:
+                companies[sym]['yahoo,exchange'].add(e)
+                stock_listed_at[sym].add(e[1])
+
+            if len(current_exchanges) == 0:
+                [current_exchanges.add(t[1]) for t in ex_variations]
+            else:
+                good = set()
+                for ex in ex_variations:
+                    if ex[1] in current_exchanges:
+                        good.add(ex[1])
+                current_exchanges = good
+
+            [exchanges.add(t[1]) for t in ex_variations]
+        current_exchanges = list(current_exchanges)
+        if len(current_exchanges) == 1:
+            index_exchange[index] = current_exchanges[0]
+        elif len(current_exchanges) > 1:
+            for p in prefs:
+                if p in current_exchanges:
+                    index_exchange[index] = p
+                    break
+        if index not in index_exchange.keys():
+            composition.pop(index, None)
+
+    exchanges = list(exchanges)
+    return exchanges, composition, index_exchange, stock_listed_at
+
+
+def insert_stocks_companies():
+    df = pd.read_csv('csv/companies.csv', sep=',')
+    stocks, companies = [], []
+
+    already = set()
+    for st in models.Stock.objects.all():
+        already.add(st.ticker)
+
+    for i, row in df.iterrows():
+        if row['ticker'] in already:
+            continue
+        st = models.Stock(ticker=row['ticker'], total_stocks=row['total_stocks'])
+        stocks.append(st)
+    if len(stocks) > 0:
+        models.Stock.objects.bulk_create(stocks)
+
+    for i, row in df.iterrows():
+        if row['ticker'] in already:
+            continue
+        if not row['logo']:
+            row['logo'] = 'https://logo.clearbit.com/clearbit.com'
+        comp = models.Company(cid=stocks[i], name=row['name'], address=row['address'], country=row['country'], logo=row['logo'], zipcode=row['zipcode'], sector=row['sector'], summary=row['summary'], city=row['city'], phone=str(row['phone']), website=row['website'], employees=row['employees'], industry=row['industry'])
+        companies.append(comp)
+    if len(companies) > 0:
+        models.Company.objects.bulk_create(companies)
+
+
+index_tickers = {'EURO STOXX 50': 'FEZ', 'SDAX': '^SDAXI', 'CAC Mid 60': '^CM100', 'DAX': '^GDAXI', 'S&P 100': '^OEX', 'OMX Stockholm 30': '^OMX', 'S&P 500': '^GSPC', 'NASDAQ 100': '^NDX', 'AEX': '^AEX', 'DOW JONES': '^DJI', 'IBEX 35': '^IBEX', 'MOEX': 'IMOEX.ME', 'BEL 20': '^BFX', 'MDAX': '^MDAXI', 'CAC 40': '^FCHI', 'TECDAX': '^TECDAX', 'FTSE 100': '^FTSE', 'OMX Helsinki 25': '^OMXH25'}
+
+
+def insert_exchanges_indices_partof_listedat():
+    exchanges, composition, index_exchange, stock_listed_at = get_index_data()
+
+    exchange_map = {}
+    exchange_objs = []
+    for ex_name in exchanges:
+        ex = models.Exchange(name=ex_name)
+        exchange_objs.append(ex)
+        exchange_map[ex_name] = ex
+    models.Exchange.objects.bulk_create(exchange_objs)
+
+    indices = []
+    indices_map = {}
+    for index, ex in index_exchange.items():
+        idx = models.Indices(eid=exchange_map[ex], index_name=index, ticker=index_tickers[index])
+        indices.append(idx)
+        indices_map[index] = idx
+    models.Indices.objects.bulk_create(indices)
+
+    ticker_id = {}
+    listedats = []
+    for st in models.Stock.objects.all():
+        ticker_id[st.ticker] = st
+        listed_at_exchange = None
+        if st.ticker not in stock_listed_at.keys():
+            for index, tickers in composition.items():
+                if st.ticker in tickers:
+                    listed_at_exchange = [index_exchange[index]]
+                    break
+            if listed_at_exchange is None:
+                listed_at_exchange = [random.choice(exchanges)]
+        else:
+            listed_at_exchange = stock_listed_at[st.ticker]
+        for ex_name in listed_at_exchange:
+            listed = models.ListedAt(sid=st, eid=exchange_map[ex_name])
+            listedats.append(listed)
+    models.ListedAt.objects.bulk_create(listedats)
+
+    partofs = []
+    for index, tickers in composition.items():
+        idx = indices_map[index]
+        for ticker in set(tickers):
+            if ticker not in ticker_id.keys():
+                continue
+            stock = ticker_id[ticker]
+            part = models.PartOfIndex(sid=stock, iid=idx)
+            partofs.append(part)
+    models.PartOfIndex.objects.bulk_create(partofs)
+
+
+insert_stocks_companies()
+insert_exchanges_indices_partof_listedat()
---
+++ data/insert_user_related_data.py
@@ -0,0 +1,204 @@
+import sys, os, django, csv
+
+project_dir = "../"
+sys.path.append(project_dir)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StockMarket.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+import market.models as models
+import pandas as pd
+from tqdm import tqdm
+import random
+from datetime import datetime
+from django.utils.timezone import make_aware
+
+
+def insert_user_person_client_broker():
+    df = pd.read_csv('csv/persons.csv', sep=',', nrows=1000)
+    users, persons = [], []
+    for i, row in df.iterrows():
+        email = row['EmailAddress']
+        username = email.split('@')[0]
+        password = username
+        name = row['GivenName'] + ' ' + row['MiddleInitial'] + '. ' + row['Surname']
+        address = row['StreetAddress'] + ', ' + row['City']
+        telephone = row['TelephoneNumber'].replace('-', '')
+        user = User(username=username, email=email, password=password, first_name=name)
+        person = models.Person(name=name, address=address, telephone=telephone)
+        users.append(user)
+        persons.append(person)
+
+    User.objects.bulk_create(users)
+    models.Person.objects.bulk_create(persons)
+
+    clients, brokers = [], []
+    for user, person in zip(users, persons):
+        # 20% of people are brokers
+        if random.randint(0, 10) < 2:
+            # is broker, add registeredat
+            commission = random.randint(3, 15) + (random.randint(0, 100) / 100)
+            broker = models.Broker(bid=person, email=user.email, commission=commission)
+            brokers.append(broker)
+        else:
+            # is client, add portfolio, holdings
+            client = models.Client(clid=person, email=user.email)
+            clients.append(client)
+
+    models.Client.objects.bulk_create(clients)
+    models.Broker.objects.bulk_create(brokers)
+
+
+def insert_reg_at():
+    regats = []
+    exchanges = list(models.Exchange.objects.all())
+    for broker in models.Broker.objects.all():
+        num = random.randint(1, len(exchanges))
+        for ex in random.sample(exchanges, num):
+            cur = models.RegisteredAt(bid=broker, eid=ex)
+            regats.append(cur)
+    models.RegisteredAt.objects.bulk_create(regats)
+
+
+def insert_stockprice(batch_size=100000):
+    ticker_obj = {}
+    for st in models.Stock.objects.all():
+        ticker_obj[st.ticker] = st
+    exchange_obj = {}
+    for ex in models.Exchange.objects.all():
+        exchange_obj[ex.name] = ex
+
+    prices = []
+    with open('csv/prices_30day.csv', 'r') as fp:
+        reader = csv.reader(fp)
+        header = None
+        for row in reader:
+            if header is None:
+                header = {}
+                for i, h in enumerate(row):
+                    header[h] = i
+            else:
+                stock = row[header['ticker']]
+                exchange = row[header['exchange']]
+                stamp = row[header['creation_time']]
+                price = float(row[header['price']])
+                if stock not in ticker_obj.keys():
+                    continue
+                if exchange not in exchange_obj.keys():
+                    continue
+                stock = ticker_obj[stock]
+                exchange = exchange_obj[exchange]
+                stamp = make_aware(datetime.strptime(stamp, '%Y-%m-%d %H:%M:%S'))
+                cur = models.StockPriceHistory(sid=stock, eid=exchange, creation_time=stamp, price=price)
+                if len(prices) >= batch_size:
+                    models.StockPriceHistory.objects.bulk_create(prices, ignore_conflicts=True)
+                    print(f'Saved {batch_size} rows')
+                    prices = []
+                prices.append(cur)
+    if len(prices) > 0:
+        models.StockPriceHistory.objects.bulk_create(prices, ignore_conflicts=True)
+
+
+def insert_last_prices():
+    exchanges = list(models.Exchange.objects.all())
+    stocks = list(models.Stock.objects.all())
+    for st in tqdm(stocks):
+        try:
+            price = models.StockPriceHistory.timescale.filter(sid=st.sid).latest().price
+        except Exception as e:
+            ex = random.choice(exchanges)
+            stamp = make_aware(datetime.now())
+            price = random.randint(30, 90) + (random.randint(0, 100) / 100)
+            cur = models.StockPriceHistory(sid=st, eid=ex, creation_time=stamp, price=price)
+            cur.save()
+        finally:
+            st.last_price = price
+            st.save(update_fields=['last_price'])
+
+    indices = list(models.Indices.objects.all())
+    for idx in tqdm(indices):
+        try:
+            price = models.IndexPriceHistory.timescale.filter(iid=idx.iid).latest().price
+        except Exception as e:
+            stamp = make_aware(datetime.now())
+            price = random.randint(30, 90) + (random.randint(0, 100) / 100)
+            cur = models.IndexPriceHistory(iid=idx, creation_time=stamp, price=price)
+            cur.save()
+        finally:
+            idx.last_price = price
+            idx.save(update_fields=['last_price'])
+
+
+def insert_indexprice(batch_size=100000):
+    index_obj = {}
+    for idx in models.Indices.objects.all():
+        index_obj[idx.ticker] = idx
+
+    prices = []
+    with open('csv/index_prices.csv', 'r') as fp:
+        reader = csv.reader(fp)
+        header = None
+        for row in reader:
+            if header is None:
+                header = {}
+                for i, h in enumerate(row):
+                    header[h] = i
+            else:
+                index = row[header['ticker']]
+                stamp = row[header['creation_time']]
+                price = float(row[header['price']])
+                if index not in index_obj.keys():
+                    continue
+                index = index_obj[index]
+                stamp = make_aware(datetime.strptime(stamp, '%Y-%m-%d %H:%M:%S'))
+                cur = models.IndexPriceHistory(iid=index, creation_time=stamp, price=price)
+                if len(prices) >= batch_size:
+                    models.IndexPriceHistory.objects.bulk_create(prices, ignore_conflicts=True)
+                    print(f'Saved {batch_size} rows')
+                    prices = []
+                prices.append(cur)
+    if len(prices) > 0:
+        models.IndexPriceHistory.objects.bulk_create(prices, ignore_conflicts=True)
+
+
+def insert_portfolio_holdings():
+    stocks = list(models.Stock.objects.all())
+    last_price, sector = {}, {}
+    for stock in tqdm(stocks):
+        last_price[stock.sid] = models.StockPriceHistory.timescale.filter(sid=stock.sid).latest().price
+        sector[stock.sid] = models.Company.objects.get(cid=stock.sid).sector
+
+    for client in models.Client.objects.all():
+        num = random.randint(10, min(len(stocks), 100))
+        cur_stocks = random.sample(stocks, num)
+
+        cur_sectors = {}
+        for st in cur_stocks:
+            if sector[st.sid] not in cur_sectors.keys():
+                cur_sectors[sector[st.sid]] = []
+            cur_sectors[sector[st.sid]].append(st)
+
+        folios = []
+        for cur_sector in cur_sectors.keys():
+            folio = models.Portfolio(clid=client, pname=cur_sector)
+            folios.append(folio)
+            cur_sectors[cur_sector] = [(st, folio) for st in cur_sectors[cur_sector]]
+        models.Portfolio.objects.bulk_create(folios)
+
+        holds = []
+        for cur_sector, st_list in cur_sectors.items():
+            for st, folio in st_list:
+                up = max(1, st.total_stocks // 1000)
+                quantity = random.randint(0, up)
+                total = quantity * last_price[st.sid]
+                hold = models.Holdings(folio_id=folio, sid=st, quantity=quantity, total_price=total)
+                holds.append(hold)
+        models.Holdings.objects.bulk_create(holds)
+
+
+insert_user_person_client_broker()
+insert_reg_at()
+insert_stockprice()
+insert_indexprice()
+insert_last_prices()
+insert_portfolio_holdings()
